5869fb1616ddb291d36afbadf9fffacba84e16f3
Chris McClelland <github.com@m3.ath.cx>
2009-08-12 20:03:41 +0100

    Ooops, I had declared led_out<0..7> but put led<0..7> in the constraint file. I also tri-state all the unused lines, and made sw(2) tri-state the used lines (to allow micro to be powered off safely whilst the FPGA is still on)

--- vhdl/fx2fpga.vhd
+++ vhdl/fx2fpga.vhd
@@ -24,21 +24,35 @@ entity fx2fpga is
 		COUNTER_WIDTH: natural := 18
 	);
 	port(
-		reset : in std_logic;
-		ifclk : in std_logic;
-		fifoAddr : out std_logic_vector(1 downto 0);
-		flagA : out std_logic;  -- These are really inputs, but tristate outs are good enough
-		flagB : out std_logic;
-		gotData : in std_logic;
-		slrd : out std_logic;
-		sloe : out std_logic;
-		slwr : out std_logic;
-		pktEnd : out std_logic;
-		sseg : out std_logic_vector(7 downto 0);
-		anode : out std_logic_vector(3 downto 0);
-		fd : in std_logic_vector(7 downto 0);
-		sw : in std_logic_vector(1 downto 0);
-		led_out : out std_logic_vector(7 downto 0)
+		reset    : in std_logic;
+		ifclk    : in std_logic;
+
+		-- Unused connections must be configured as inputs
+		flagA    : out std_logic;
+		flagB    : out std_logic;
+		int0     : out std_logic;                     -- PA0
+		int1     : out std_logic;                     -- PA1
+		pa3      : out std_logic;                     -- PA3
+		pa7      : out std_logic;                     -- PA7
+		clkout   : out std_logic;
+		pd       : out std_logic_vector(7 downto 0);
+
+		-- Data & control from the FX2
+		fd       : in std_logic_vector(7 downto 0);
+		gotData  : in std_logic;                     -- FLAGC
+
+		-- Control to the FX2
+		sloe     : out std_logic;                    -- PA2
+		slrd     : out std_logic;
+		slwr     : out std_logic;
+		fifoAddr : out std_logic_vector(1 downto 0); -- PA4 & PA5
+		pktEnd   : out std_logic;                    -- PA6
+
+		-- Onboard peripherals
+		sseg     : out std_logic_vector(7 downto 0);
+		anode    : out std_logic_vector(3 downto 0);
+		sw       : in std_logic_vector(2 downto 0);
+		led_out  : out std_logic_vector(7 downto 0)
 	);
 end fx2fpga;
 
@@ -46,7 +60,6 @@ architecture arch of fx2fpga is
 	signal counter, counter_next: unsigned(COUNTER_WIDTH-1 downto 0);
 	signal hex: std_logic_vector(3 downto 0);
 	signal checksum, checksum_next: unsigned(15 downto 0);
-	--signal oldValue, oldValue_next: std_logic_vector(7 downto 0);
 	signal led, led_next: std_logic_vector(7 downto 0);
 begin
 	process(ifclk, reset)
@@ -55,32 +68,34 @@ begin
 			counter <= (others => '0');
 			checksum <= (others => '0');
 			led <= (others => '0');
-			--oldValue <= (others => '0');
 		elsif ( ifclk'event and ifclk = '1' ) then
 			counter <= counter_next;
 			checksum <= checksum_next;
 			led <= led_next;
-			--oldValue <= oldValue_next;
 		end if;
 	end process;
 
 	-- binary counter
 	counter_next <= counter + 1;
 	
-	led_out <= led;
-	--oldValue_next <= fd;
-	
-	fifoAddr <= sw;
+	-- Tri-state all the unused lines
 	flagA <= 'Z';
 	flagB <= 'Z';
-	sloe <= '0';
-	slrd <= '0';
-	slwr <= '1';
-	pktEnd <= '1';
+	int0 <= 'Z';
+	int1 <= 'Z';
+	pa3 <= 'Z';
+	pa7 <= 'Z';
+	clkout <= 'Z';
+	pd <= (others => 'Z');
+	
+	led_out <= led;
 
-	--checksum_next <=
-	--	checksum + 1 when gotData = '1' and oldValue /= fd
-	--	else checksum;
+	fifoAddr <= sw(1 downto 0) when sw(2) = '1' else (others => 'Z');
+	sloe <= '0'                when sw(2) = '1' else 'Z';
+	slrd <= '0'                when sw(2) = '1' else 'Z';
+	slwr <= '1'                when sw(2) = '1' else 'Z';
+	pktEnd <= '1'              when sw(2) = '1' else 'Z';
+
 	checksum_next <=
 		checksum + unsigned(fd) when gotData = '1'
 		else checksum;
