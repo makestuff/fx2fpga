ce050f7c21dc52a060c860880e8839264826d261
makestuff <github.com@m3.ath.cx>
2010-08-14 23:24:40 +0100

    Moved benchmark code to fx2tools package

--- benchmark/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cl -c -DWIN32 -I..\..\usbutil -IC:\bin\libusb-win32-device-bin-0.1.12.2\include main.c
-cl -Febenchmark.exe *.obj ..\..\usbutil\usbutil.lib c:\bin\libusb-win32-device-bin-0.1.12.2\lib\msvc\libusb.lib
-del *.obj *.pdb
